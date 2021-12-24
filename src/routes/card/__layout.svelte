0d20a608c62ce158488ec98d2dca66a97c6f7554
Anton B <tonborzenko@gmail.com>
2021-12-24 14:16:29 +0200

    Add home page

---
+++ src/routes/card/__layout.svelte
@@ -0,0 +1,7 @@
+<script type="ts">
+  import Layout from '$src/components/layout/Layout.svelte'
+</script>
+
+<Layout>
+  <slot></slot>
+</Layout>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,46 @@
-<div class="media-wrap">
-  <div class="img-box video-box text-center">
-    <video src="/media/Lemon_v_720.mp4" autoPlay muted loop playsInline />
+<script type="ts">
+  import { onMount } from 'svelte'
+  import Logo from '$src/components/layout/Logo.svelte'
+  import Auth from '$src/components/layout/Auth.svelte'
+  import near from '$src/utils/near'
+  onMount(async () => {
+    $near.connect()
+    const imageMapResize = (await import('image-map-resizer/js/imageMapResizer')).default
+    imageMapResize();
+  })
+</script>
+
+<style>
+  .link {
+    position: absolute;
+    background: transparent;
+    padding: 20px;
+    transition: 0.3s;
+    cursor:url(/img/cash-on-delivery.png), auto;
+  }
+  .logo-home {
+    position: absolute;
+    top: 4%;
+    left: 4%;
+  }
+  .auth-home {
+    position: absolute;
+    top: 5%;
+    right: 4%;
+  }
+</style>
+
+
+<div style="position: relative">
+  <a href="/" class="logo-home" style="color: #f0f0f0;">
+    <Logo height={50} />
+  </a>		
+
+  <div class="auth-home">
+    <Auth />
   </div>
+
+  <img src="/img/home_main_image.jpg" usemap="#image-map" alt="Home page" class="img-fluid">
+
+  <a href="/shop" class="link clip" style="left: 71.4%; top: 20.8%; width: 28%; height: 45%;">&nbsp;</a>
 </div>
